aa708aff9e3d6012282fa89e3ca37004a932c7b0
hallier <ahallier@isara.fr>
2021-11-08 18:09:17 +0100

    Test : controller modify test of controller

--- TourGuide/src/main/java/tourGuide/TourGuideModule.java
+++ TourGuide/src/main/java/tourGuide/TourGuideModule.java
@@ -4,15 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import rewardCentral.RewardCentral;
-import tourGuide.rewards.service.RewardsService;
 
 @Configuration
 public class TourGuideModule {
-	
-	@Bean
-	public RewardsService getRewardsService() {
-		return new RewardsService(getRewardCentral());
-	}
 	
 	@Bean
 	public RewardCentral getRewardCentral() {
--- TourGuide/src/test/java/tourGuide/RewardsControllerTest.java
+++ TourGuide/src/test/java/tourGuide/RewardsControllerTest.java
@@ -1,14 +1,17 @@
 package tourGuide;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
-import tourGuide.rewards.controller.RewardsController;
 import tourGuide.rewards.service.RewardsService;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -16,7 +19,9 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = RewardsController.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class RewardsControllerTest {
 
   @Autowired
@@ -28,7 +33,11 @@ public class RewardsControllerTest {
   @Test
   public void getRewards_shouldReturnOk() throws Exception {
     when(rewardsService.getRewardPoints(any(UUID.class), any(UUID.class))).thenReturn(10);
-    mockMvc.perform(get("/rewards")).andExpect(status().isOk());
+    mockMvc.perform(get("/rewards")
+            .contentType(MediaType.APPLICATION_JSON)
+            .param("attractionId", "6ba7b810-9dad-11d1-80b4-00c04fd430c8")
+            .param("userId", "6ba7b810-9dad-11d1-80b4-00c04fd430c8"))
+            .andExpect(status().isOk());
   }
 
 }
